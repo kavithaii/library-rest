b5fcb530feb01fd3eb3a9303f7dd3419fb453c6e
Kavitha <kavitha@m5nv.com>
2021-04-12 15:57:08 +0530

    add test for GET endpoint

--- crud.test.js
+++ crud.test.js
@@ -8,6 +8,12 @@ chai.use(chaiHttp);
 const apiServer = server.app.listen(server.port);
 const Api = () => chai.request(apiServer);
 
+const books = [
+    { id: 23, author: "Orange", title: "The book tour", isbn: "978-3-16-148410-0", releaseDate: "12-09-2000" },
+    { id: 12, author: "Shay", title: "Coin-up", isbn: "978-1-60309-427-6", releaseDate: "26-01-2006" },
+    { id: 103, author: "Hila", title: "The Dragon", isbn: "978-1-60309-057-5", releaseDate: "04-05-2010" },
+];
+
 describe('unit test library REST API', () => {
     before(async () => {
       console.log(`API server is listening on port ${server.port}`);
@@ -24,5 +30,21 @@ describe('unit test library REST API', () => {
             expect(res.status).to.equal(200);
             expect(res.text).to.equal('Hi there');
         });
+    });
+
+    context('test the rest end points', () => {
+        it('should GET list of all available books in library', async function () {
+            const res = await Api().get('/books');
+            expect(res.status).to.equal(200);
+            // console.log('>>>', res.body.length);
+            // console.log('>>>', books.length);
+            expect(res.body.length).to.equal(books.length);
+        });
+
+        it('should GET a book from library using id', async function () {
+            const id = 23
+            const res = await Api().get(`/books/${id}`);
+            expect(res.status).to.equal(200);
+        })
     });
 });
--- server.js
+++ server.js
@@ -2,14 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const books = [
+    { id: 23, author: "Orange", title: "The book tour", isbn: "978-3-16-148410-0", releaseDate: "12-09-2000" },
+    { id: 12, author: "Shay", title: "Coin-up", isbn: "978-1-60309-427-6", releaseDate: "26-01-2006" },
+    { id: 103, author: "Hila", title: "The Dragon", isbn: "978-1-60309-057-5", releaseDate: "04-05-2010" },
+];
+
 app.get('/hello', (req, res) => {
     res.send('Hi there');
 });
 
+app.get("/books", (req, res) => {
+    res.json(books);
+});
+  
+app.get("/books/:id", (req, res) => {
+    const { id } = req.params
+    const oneBook = books.filter(
+      item => item.id === (+id)
+    )
+    console.log(oneBook);
+    res.json(oneBook);
+});
+
 // launch the server and listen only when running as a standalone process
 if (!module.parent) {
-      // start listening iff all good... we get here only if the database exists
-      // and proxy user is either created anew or already exists.
+      // start listening iff all good... 
       app.listen(port, async () => {
         console.log(
           `Conduits API server is listening on port ${port}`
